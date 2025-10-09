e6c4065299c2c6309da2ad8d752742f1e09b259d
Charlie Moss <mossch6@furman.edu>
2025-10-09 17:05:26 -0400

    Some more implementation, however will use the one on main instead

--- src/AST/AST.cs
+++ src/AST/AST.cs
@@ -1,22 +1,69 @@
 namespace AST
 {
-
-    public class LiteralNode { }
-    public class VariableNode { }
-
     public abstract class ExpressionNode { }
     public abstract class Operator : ExpressionNode { }
-    public abstract class BinaryOperator : Operator { }
-    public class PlusNode : BinaryOperator { }
-    public class MinusNode : BinaryOperator { }
-    public class TimesNode : BinaryOperator { }
+    public abstract class BinaryOperator : Operator
+    {
+        public ExpressionNode? Left { get; set; }
+        public ExpressionNode? Right { get; set; }
+        public object Data { get; set; }
+
+        public BinaryOperator()
+        {
+            Left = null;
+            Right = null;
+        }
+    }
+
+    public class PlusNode : BinaryOperator
+    {
+        public PlusNode() : base()
+        {
+            Data = "+";
+        }
+    }
+    public class MinusNode : BinaryOperator
+    {
+        public MinusNode() : base()
+        {
+            Data = "-";
+        }
+    }
+    public class TimesNode : BinaryOperator
+    { 
+        public TimesNode() : base()
+        {
+            Data = "*";
+        }
+    }
     public class FloatDivNode : BinaryOperator { }
     public class IntDivNode : BinaryOperator { }
     public class ModulusNode : BinaryOperator { }
     public class ExponentiationNode : BinaryOperator { }
 
+    public class LiteralNode : ExpressionNode
+    {
+        public object Value { get; set; }
+        public LiteralNode(object val)
+        {
+            Value = val;
+        }
+    }
+
+    public class VariableNode : ExpressionNode
+    {
+        public string Name { get; set; }
+        public VariableNode(string str)
+        {
+            Name = str;
+        }
+    }
+
     public abstract class Statement { }
-    public class BlockStmt : Statement { }
+    public class BlockStmt : Statement
+    {
+        SymbolTable<string, object> symbolTable = new SymbolTable<string, object>();
+    }
     public class AssignmentStmt : Statement { }
     public class ReturnStmt : Statement { }
     
