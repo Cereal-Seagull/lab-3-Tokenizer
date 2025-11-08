5790c19f3ac5d171e5f185b546ce79b30fbdc3b0
Charlie Moss <mossch6@furman.edu>
2025-11-07 20:02:46 -0500

    NameAnalysisVistor methods implementation

--- src/AST/Visitors/NameAnalysisVisitor.cs
+++ src/AST/Visitors/NameAnalysisVisitor.cs
@@ -1,46 +1,57 @@
+using System.Drawing;
 using AST;
 
 public class NameAnalysisVisitor : IVisitor<Tuple<SymbolTable<string, object>, Statement>, bool>
-{
-
-    // WILL USE EXCEPTIONS!!!!
-    // Analyzes ENTIRE tree regardless of errors or returns
-    
+{   
     #region Binary Operator nodes
 
     public bool Visit(PlusNode n, Tuple<SymbolTable<string, object>, Statement> p)
     {
-        throw new NotImplementedException();
+        bool left = n.Left.Accept(this, p);
+        bool right = n.Right.Accept(this, p);
+        return left && right;
     }
 
     public bool Visit(MinusNode n, Tuple<SymbolTable<string, object>, Statement> p)
     {
-        throw new NotImplementedException();
+        bool left = n.Left.Accept(this, p);
+        bool right = n.Right.Accept(this, p);
+        return left && right;
     }
 
     public bool Visit(TimesNode n, Tuple<SymbolTable<string, object>, Statement> p)
     {
-        throw new NotImplementedException();
+        bool left = n.Left.Accept(this, p);
+        bool right = n.Right.Accept(this, p);
+        return left && right;
     }
 
     public bool Visit(FloatDivNode n, Tuple<SymbolTable<string, object>, Statement> p)
     {
-        throw new NotImplementedException();
+        bool left = n.Left.Accept(this, p);
+        bool right = n.Right.Accept(this, p);
+        return left && right;
     }
 
     public bool Visit(IntDivNode n, Tuple<SymbolTable<string, object>, Statement> p)
     {
-        throw new NotImplementedException();
+        bool left = n.Left.Accept(this, p);
+        bool right = n.Right.Accept(this, p);
+        return left && right;
     }
 
     public bool Visit(ExponentiationNode n, Tuple<SymbolTable<string, object>, Statement> p)
     {
-        throw new NotImplementedException();
+        bool left = n.Left.Accept(this, p);
+        bool right = n.Right.Accept(this, p);
+        return left && right;
     }
     
     public bool Visit(ModulusNode n, Tuple<SymbolTable<string, object>, Statement> p)
     {
-        throw new NotImplementedException();
+        bool left = n.Left.Accept(this, p);
+        bool right = n.Right.Accept(this, p);
+        return left && right;
     }
 
     #endregion
@@ -49,12 +60,12 @@ public class NameAnalysisVisitor : IVisitor<Tuple<SymbolTable<string, object>, S
 
     public bool Visit(LiteralNode n, Tuple<SymbolTable<string, object>, Statement> p)
     {
-        throw new NotImplementedException();
+        return true;
     }
 
     public bool Visit(VariableNode n, Tuple<SymbolTable<string, object>, Statement> p)
     {
-        throw new NotImplementedException();
+        return p.Item1.ContainsKey(n.Name);
     }
 
     #endregion
@@ -63,15 +74,15 @@ public class NameAnalysisVisitor : IVisitor<Tuple<SymbolTable<string, object>, S
 
     public bool Visit(AssignmentStmt n, Tuple<SymbolTable<string, object>, Statement> p)
     {
-        throw new NotImplementedException();
-    }
-
-    public bool Visit(BlockStmt n, Tuple<SymbolTable<string, object>, Statement> p)
-    {
-        throw new NotImplementedException();
+        return n.Variable.Accept(this, p) && n.Expression.Accept(this, p);
     }
 
     public bool Visit(ReturnStmt n, Tuple<SymbolTable<string, object>, Statement> p)
+    {
+        return n.Expression.Accept(this, p);
+    }
+    
+    public bool Visit(BlockStmt n, Tuple<SymbolTable<string, object>, Statement> p)
     {
         throw new NotImplementedException();
     }
